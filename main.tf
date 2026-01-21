1f579db80471f0d3a2f76db6171351b4eaa0f090
Lalitha-SR <145996677+Lalitha-SR@users.noreply.github.com>
2026-01-21 10:22:26 +0530

    Fix key_name formatting in AWS instance resources
    Updated key_name values to use quotes for consistency.

--- main.tf
+++ main.tf
@@ -205,7 +205,7 @@ resource "aws_instance" "web1" {
   ami                         = "ami-02dc6e3e481e2bbc5"
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.pub_sub1.id
-  key_name                    = demo
+  key_name                    = "demo"
   associate_public_ip_address = "true"
   vpc_security_group_ids      = [aws_security_group.sg1.id]
 
@@ -220,7 +220,7 @@ resource "aws_instance" "db" {
   ami                    = "ami-02dc6e3e481e2bbc5"
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.pri_sub1.id
-  key_name               = demo
+  key_name               = "demo"
   vpc_security_group_ids = [aws_security_group.sg1.id]
 
   tags = {
