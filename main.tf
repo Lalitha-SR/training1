f07658f07d3e4138c507a26f5607a33a59e6ca47
Lalitha-SR <145996677+Lalitha-SR@users.noreply.github.com>
2026-01-21 11:35:42 +0530

    Change availability zone for public subnet

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_subnet" "pub_sub1" {
 resource "aws_subnet" "pub_sub2" {
   vpc_id                  = aws_vpc.vpc1.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "us-west-1"
   map_public_ip_on_launch = true
 
   tags = {
