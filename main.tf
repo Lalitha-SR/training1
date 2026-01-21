f475ed27cfaa245279be5572b15138438822f0e3
Lalitha-SR <145996677+Lalitha-SR@users.noreply.github.com>
2026-01-21 12:03:22 +0530

    Use dynamic availability zones for public subnets

--- main.tf
+++ main.tf
@@ -20,10 +20,14 @@ resource "aws_internet_gateway" "igw1" {
 }
 
 # 3. Public subnet 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
 resource "aws_subnet" "pub_sub1" {
   vpc_id                  = aws_vpc.vpc1.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 
   tags = {
@@ -33,7 +37,7 @@ resource "aws_subnet" "pub_sub1" {
 resource "aws_subnet" "pub_sub2" {
   vpc_id                  = aws_vpc.vpc1.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-1d"
+  availability_zone       = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = true
 
   tags = {
