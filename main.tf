be8e1939e00c40af52fcf1f0d9df166515372370
Lalitha-SR <145996677+Lalitha-SR@users.noreply.github.com>
2026-01-21 10:33:13 +0530

    Fix associate_public_ip_address value type

--- main.tf
+++ main.tf
@@ -206,7 +206,7 @@ resource "aws_instance" "web1" {
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.pub_sub1.id
   key_name                    = "demo"
-  associate_public_ip_address = "true"
+  associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg1.id]
 
   tags = {
